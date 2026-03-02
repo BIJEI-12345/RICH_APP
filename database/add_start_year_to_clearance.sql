238aeb2216b97ee1e90b8de7a7450a93fb993ea6
BIJEI-12345 <bijei-12345@users.noreply.github.com>
2026-03-02 21:11:09 +0800

    Remove database folder from repository

--- database/add_start_year_to_clearance.sql
+++
@@ -1,5 +0,0 @@
--- Add start_year column to clearance_forms table
--- Run this SQL to update the existing database
-
-ALTER TABLE clearance_forms 
-ADD COLUMN IF NOT EXISTS start_year VARCHAR(50) DEFAULT NULL AFTER location;
